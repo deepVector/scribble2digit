9c43497c435cec7da2fd939e331048aa2215e28a
c0 <848294@gmail.com>
2020-05-17 19:37:45 -0400

    Add data, models and utils modules

---
+++ scribble2digit/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""TODO"""
+
+__author__ = "deepVector: https://github.com/deepVector"
+__copyright__ = "Copyright (c) 2020-present, deepVector"
+__credits__ = [""]
+__license__ = "Creative Commons Attribution 4.0 International (cc-by-4.0)"
+__maintainer__ = "deepVector"
+
